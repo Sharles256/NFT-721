a702db358bb9d7bce7063da1ce37acbf875f3edf
Sharles256 <108944934+Sharles256@users.noreply.github.com>
2022-07-30 14:04:10 -0500

    Update EPPRNFT.sol

--- EPPRNFT.sol
+++ EPPRNFT.sol
@@ -10,17 +10,18 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 contract EPPRNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
-    
+
+    address contractOwner;
+    //  Tarea 1 en esta variable address el contrato se entera quien es su papi
 
     constructor() ERC721("EPPR NFT", "MI PRIMER NFT") {
-    //Aquí se define quien es el dueño del contrato
+    //  Aquí se define quien es el dueño del contrato
     contractOwner = msg.sender;
     }
 
-    address contractOwner;
-    //Tarea 1 en esta variable address el contrato se entera quien es su papi
 
-//function totalSupply() public view returns(uint256){ return _tokenIds;      }
+
+    function totalSupply() public view  returns(uint256){ return _tokenIds.current();      }
 
     event Mint(
         address indexed sender,
@@ -29,35 +30,41 @@ contract EPPRNFT is ERC721URIStorage {
         uint256 tokenId
     );
 
-    function mint(address owner, string memory tokenURI)
+    function mint(address _address, string memory _tokenURI)
         public
         returns (uint256)
     {
         if(_address != msg.sender){
-        require( 
-            msg.sender == contractOwner, 
-            "Only contract owner can AIRDROP NFT"
-        );
-//Tarea 2 Permitir hacer AIRDROPS  
+            //  Airdrop está aquí
+            require(
+                msg.sender == contractOwner,
+                "Only contract owner can Airdrop NFT"
+            );
+        }
+        else{
+            //  MINT regular aquí
+        }
+        
+        //  Tarea 2 Permitir hacer AIRDROPS  
 
         _tokenIds.increment();
 
         uint256 newItemId = _tokenIds.current();
-        _mint(owner, newItemId);
-        _setTokenURI(newItemId, tokenURI);
+        _mint(_address, newItemId);
+        _setTokenURI(newItemId, _tokenURI);
 
-        emit Mint(msg.sender, owner, tokenURI, newItemId);
+        emit Mint(msg.sender, _address, _tokenURI, newItemId);
         return newItemId;
     }
 
-    //Tarea 3 crear función para poder modificar el Json y que solo el dueño del contrato lo pueda hacer
+    //  Tarea 3 crear función para poder modificar el Json y que solo el dueño del contrato lo pueda hacer
     function setUri(uint256 _tokenId, string memory  _URI) external {
         require(
             msg.sender == contractOwner,
             "Only contract owner can change TokenId or URI"
             );
          _setTokenURI(_tokenId, _URI);
-}
+        }
 
     function uri(uint256 tokenId) public view returns (string memory) {
         return tokenURI(tokenId);
